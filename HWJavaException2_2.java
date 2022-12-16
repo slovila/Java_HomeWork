4c8aa8aecd700b07ef7e54c37fd2ca0d21ed6b9f
Alexey Nikitenko <slovila.kit51@gmail.com>
2022-12-16 15:56:52 +0300

    Task2

---
+++ HWJavaException2_2.java
@@ -0,0 +1,16 @@
+
+
+
+import java.sql.Array;
+public class HWJavaException2_2 {
+    public static void main(String[] args) {
+        try {
+            int d = 0;
+            int[] intArray = {1, 5, 6, 8, 7, 9, 2, 1, 9};
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+    }
+}
