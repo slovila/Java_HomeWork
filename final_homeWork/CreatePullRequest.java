00bea44f14f4bfb1d0ebf1ef355bfe3c6cc09809
Alexey Nikitenko <slovila.kit51@gmail.com>
2023-03-15 15:00:18 +0300

    final HW

---
+++ final_homeWork/CreatePullRequest.java
@@ -0,0 +1,4 @@
+package final_homeWork;
+
+public class CreatePullRequest {
+}
---
+++ final_homeWork/DateException.java
@@ -0,0 +1,12 @@
+package final_homeWork;
+
+public class DateException extends Exception {
+    public DateException() {
+    }
+
+    public void dataException(String i) {
+        System.out.println("Exception: DateNotCorrectFormat");
+        System.out.printf("It`s not correct date format: %s", i);
+        System.out.println();
+    }
+}
